3035f81b7f4a4b53b4553742ffd3855700d2cfed
Liquelaliqour <lewismomanyi34@gmail.com>
2025-09-19 16:38:25 +0300

    feat: implement UserCard component and dynamic users page with static props

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,13 +1,39 @@
 import React from "react";
 import Header from "@/components/layout/Header";
+import UserCard from "../../components/common/UserCard";
+import { UserProps } from "../../interfaces";
 
-const Users: React.FC = () => {
+interface UsersPageProps {
+  posts: UserProps[];
+}
+
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
   return (
-    <div>
+    <div className="flex flex-col h-screen">
       <Header />
-      <div>Users Page</div>
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Users</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {posts?.map((user: UserProps, key: number) => (
+            <UserCard key={key} {...user} />
+          ))}
+        </div>
+      </main>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
 
 export default Users;
